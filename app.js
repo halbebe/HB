d24c080a7ef5b3445bd05adb6bc38eec9f64824e
halbebe <gksmfsf97@naver.com>
2023-11-20 10:20:43 +0900

    러지컷

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
-const express = require('express');
+const express = require("express");
+const cookieParser = require("cookie-parser");
 const app = express();
-const port = 3000;
+
+const productRouter = require("./routers/products.js");
+const userRouter = require("./routers/users.js");
+require('dotenv').config();
+
+app.use(express.json());
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+  res.send("안녕하세요 세계!");
+})
 
-app.listen(port, () => {
-  console.log(port, '포트로 서버가 열렸어요!');
-});
+app.use("/api",[userRouter,productRouter]);
+
+app.listen(process.env.PORT, () => {
+  console.log( '서버가 실행되었습니다.');
+})
